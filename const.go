de287f26c716a3e9bb8e5918a854f268fb09443c
Matteo Vollero <m.vollero96@gmail.com>
2024-11-27 19:08:11 +0100

    Updated constants by adding more order statuses for future implementation

--- const.go
+++ const.go
@@ -2,6 +2,18 @@ package binance_client
 
 type OrderStatus string
 
+// @remind : For now NEW and PARTIALLY_FILLED are not used, but they are implemented since in future they may be hepful
+// The future scenario can be like:
+// if PARTIALLY_FILLED then
+//
+//	cancel order
+//
+// endif
+// Sell in profit what I have bought
+// Same logic applies to when selling, of course if done with selling can happen that
+// the profit are not exactly what we expect but something less
 const (
-	FILLED OrderStatus = "FILLED"
+	NEW              OrderStatus = "NEW"
+	FILLED           OrderStatus = "FILLED"
+	PARTIALLY_FILLED OrderStatus = "PARTIALLY_FILLED"
 )
--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/monkeymatt0/binance_client
 
 go 1.21.3
 
-require github.com/monkeymatt0/binance_url_builder v1.0.8-beta
+require github.com/monkeymatt0/binance_url_builder v1.0.9-beta
 
-require github.com/gorilla/websocket v1.5.3 // indirect
+require github.com/gorilla/websocket v1.5.3
